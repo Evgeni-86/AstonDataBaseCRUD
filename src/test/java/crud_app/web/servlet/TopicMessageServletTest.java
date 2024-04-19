8ae27a0cb535fea2a120974ad085f2b891c4b1e6
evgeni batanov <batanovevgeni@mail.ru>
2024-04-19 13:20:23 +0300

    refactoring code and add tests

---
+++ src/test/java/crud_app/web/servlet/TopicMessageServletTest.java
@@ -0,0 +1,24 @@
+package crud_app.web.servlet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopicMessageServletTest {
+
+    @Test
+    void doGet() {
+    }
+
+    @Test
+    void doPost() {
+    }
+
+    @Test
+    void doPut() {
+    }
+
+    @Test
+    void doDelete() {
+    }
+}
---
+++ src/test/java/crud_app/web/servlet/TopicServletTest.java
@@ -0,0 +1,28 @@
+package crud_app.web.servlet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopicServletTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void doGet() {
+    }
+
+    @Test
+    void doPost() {
+    }
+
+    @Test
+    void doPut() {
+    }
+
+    @Test
+    void doDelete() {
+    }
+}
